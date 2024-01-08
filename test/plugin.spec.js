1ebef27377b383c728d95f6216fc7ccf1bd3a5c7
saikrishna321 <saikrishna321@yahoo.com>
2024-01-08 16:36:12 +0800

    bump version

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -26,65 +26,64 @@ describe('Plugin Test', () => {
     const mockId = await driver.execute('interceptor: addMock', {
       config: {
         url: '/api/users?.*',
-        responseBody:
-          JSON.stringify({
-            page: 1,
-            per_page: 6,
-            total: 12,
-            total_pages: 2,
-            data: [
-              {
-                id: 1,
-                email: 'saikrishna.bluth@reqres.in',
-                first_name: 'George',
-                last_name: 'Bluth',
-                avatar: 'https://reqres.in/img/faces/1-image.jpg',
-              },
-              {
-                id: 2,
-                email: 'janet.weaver@reqres.in',
-                first_name: 'Janet',
-                last_name: 'Weaver',
-                avatar: 'https://reqres.in/img/faces/2-image.jpg',
-              },
-              {
-                id: 3,
-                email: 'emma.wong@reqres.in',
-                first_name: 'Emma',
-                last_name: 'Wong',
-                avatar: 'https://reqres.in/img/faces/3-image.jpg',
-              },
-              {
-                id: 4,
-                email: 'eve.holt@reqres.in',
-                first_name: 'Eve',
-                last_name: 'Holt',
-                avatar: 'https://reqres.in/img/faces/4-image.jpg',
-              },
-              {
-                id: 5,
-                email: 'charles.morris@reqres.in',
-                first_name: 'Charles',
-                last_name: 'Morris',
-                avatar: 'https://reqres.in/img/faces/5-image.jpg',
-              },
-              {
-                id: 6,
-                email: 'tracey.ramos@reqres.in',
-                first_name: 'Tracey',
-                last_name: 'Ramos',
-                avatar: 'https://reqres.in/img/faces/6-image.jpg',
-              },
-            ],
-            support: {
-              url: 'https://reqres.in/#support-heading',
-              text: 'To keep ReqRes free, contributions towards server costs are appreciated!',
+        responseBody: JSON.stringify({
+          page: 1,
+          per_page: 6,
+          total: 12,
+          total_pages: 2,
+          data: [
+            {
+              id: 1,
+              email: 'saikrishna.bluth@reqres.in',
+              first_name: 'George',
+              last_name: 'Bluth',
+              avatar: 'https://reqres.in/img/faces/1-image.jpg',
             },
-          }),
+            {
+              id: 2,
+              email: 'janet.weaver@reqres.in',
+              first_name: 'Janet',
+              last_name: 'Weaver',
+              avatar: 'https://reqres.in/img/faces/2-image.jpg',
+            },
+            {
+              id: 3,
+              email: 'emma.wong@reqres.in',
+              first_name: 'Emma',
+              last_name: 'Wong',
+              avatar: 'https://reqres.in/img/faces/3-image.jpg',
+            },
+            {
+              id: 4,
+              email: 'eve.holt@reqres.in',
+              first_name: 'Eve',
+              last_name: 'Holt',
+              avatar: 'https://reqres.in/img/faces/4-image.jpg',
+            },
+            {
+              id: 5,
+              email: 'charles.morris@reqres.in',
+              first_name: 'Charles',
+              last_name: 'Morris',
+              avatar: 'https://reqres.in/img/faces/5-image.jpg',
+            },
+            {
+              id: 6,
+              email: 'tracey.ramos@reqres.in',
+              first_name: 'Tracey',
+              last_name: 'Ramos',
+              avatar: 'https://reqres.in/img/faces/6-image.jpg',
+            },
+          ],
+          support: {
+            url: 'https://reqres.in/#support-heading',
+            text: 'To keep ReqRes free, contributions towards server costs are appreciated!',
+          },
+        }),
       },
     });
     expect(mockId).to.not.be.null;
-    const el1 = await driver.$("xpath://android.widget.TextView[@text=\"Get User List\"]");
+    const el1 = await driver.$('xpath://android.widget.TextView[@text="Get User List"]');
     await el1.click();
     const page = await driver.getPageSource();
     expect(page.includes('saikrishna.bluth@reqres.in')).to.be.true;
@@ -95,19 +94,19 @@ describe('Plugin Test', () => {
       config: {
         url: '/api/users?.*',
         updateResponseBody: [
-            {
-              jsonPath: '$.data[?(/tracey.*/.test(@.email))].first_name',
-              value: 'sudharsan',
-            },
-            {
-              jsonPath: '$.data[?(/tracey.*/.test(@.email))].last_name',
-              value: 'selvaraj',
-            },
-          ],
+          {
+            jsonPath: '$.data[?(/tracey.*/.test(@.email))].first_name',
+            value: 'sudharsan',
+          },
+          {
+            jsonPath: '$.data[?(/tracey.*/.test(@.email))].last_name',
+            value: 'selvaraj',
+          },
+        ],
       },
     });
     expect(mockId).to.not.be.null;
-    const el1 = await driver.$("xpath://android.widget.TextView[@text=\"Get User List\"]");
+    const el1 = await driver.$('xpath://android.widget.TextView[@text="Get User List"]');
     await el1.click();
     const page = await driver.getPageSource();
     expect(page.includes('sudharsan')).to.be.true;
@@ -118,11 +117,11 @@ describe('Plugin Test', () => {
     const mockId = await driver.execute('interceptor: addMock', {
       config: {
         url: '/api/users?.*',
-        statusCode: 400
+        statusCode: 400,
       },
     });
     expect(mockId).to.not.be.null;
-    const el1 = await driver.$("xpath://android.widget.TextView[@text=\"Get User List\"]");
+    const el1 = await driver.$('xpath://android.widget.TextView[@text="Get User List"]');
     await el1.click();
     const page = await driver.getPageSource();
     expect(page.includes('Error')).to.be.true;
