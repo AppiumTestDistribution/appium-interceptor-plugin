cf26fdb08857bbb9e04365ab0b0c67cbba435bcf
dmkozin <dmkozin@ozon.ru>
2026-01-21 12:56:16 +0300

    Add upstreamproxy flag

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,10 +4,12 @@ export interface IPluginArgs {
   certdirectory: string;
   whitelisteddomains: string[] | string;
   blacklisteddomains: string[] | string;
+  upstreamproxy: string | null;
 }
 
 export const DefaultPluginArgs: IPluginArgs = {
   certdirectory: path.join(__dirname, '..', 'certificate'),
   whitelisteddomains: [],
   blacklisteddomains: [],
+  upstreamproxy: null,
 };
